c2258559dd82ed7fd57de5b51dc5ce466398ed4b
CG <heartlandergames@gmail.com>
2016-04-06 21:24:06 -0500

    Added Project Folders and Base Scripts

---
+++ STSub/Assets/Scripts/Item.js
@@ -0,0 +1,74 @@
+﻿#pragma strict
+
+var id : String;
+
+var itemName : String;
+
+var type : itemType;
+
+var size : Vector3;
+
+var weight : float;
+
+var owner : Player;
+
+var quantity : float;
+
+var maxQuan : float;
+
+var quality : float;
+
+var durability : float;
+
+var maxDurability :float;
+
+var lastCheck : float;
+
+
+enum itemType
+{
+    Connector,
+    Consumable,
+    Construction,
+    Appliance,
+    Tool,
+    Container,
+    Clothing,
+
+    }
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function Item()
+{
+    itemName = "RandomizedItem";
+    type = Random.Range(0,6);
+    weight = Random.Range(.1, 50);
+    size = Vector3(Random.Range(.1,10),Random.Range(.1,10),Random.Range(.1,10));
+    maxQuan = 100;
+    quantity = Random.Range(0,maxQuan);    
+    quality = Random.Range(0,100);
+    durability = Random.Range(0,100);
+    maxDurability = 100;
+    lastCheck = Time.time;
+}
+
+function Item(nam : String, typ : int, wei : float, siz : Vector3, quan : float, mQua : float, qual : float, dur : float, mDur : float)
+    {
+        itemName = nam;
+        type = typ;
+        weight = wei;
+        size = siz;
+        quantity = quan;
+        maxQuan = mQua;
+        quality = qual;
+        durability = dur;
+        maxDurability = mDur;
+        lastCheck = Time.time;
+    }
---
+++ STSub/Assets/Scripts/MoveLook.js
@@ -0,0 +1,85 @@
+﻿#pragma strict
+
+var moveSpeed : float = 5;
+
+var lookSpeed :float = 5;
+
+var moveLock : boolean = false;
+
+var lookLock : boolean = false;
+
+var yInvertLook : boolean = true;
+
+var cameraHolder : Transform;
+
+var maxY : float = 60;
+
+var minY :float = 60;
+
+
+function Start () {
+
+}
+
+function Update () {
+    MoveKeyboard();
+    LookMouse();
+    ClampLook();
+}
+
+function MoveKeyboard ()
+{
+    if(moveLock)
+    {
+        return;
+    }
+
+    if(Input.GetButton("W"))
+    {
+        transform.Translate(transform.forward * Time.deltaTime * moveSpeed);
+    }
+    if(Input.GetButton("S"))
+    {
+        transform.Translate(-transform.forward * Time.deltaTime * moveSpeed);
+    }
+    if(Input.GetButton("D"))
+    {
+        transform.Translate(transform.right * Time.deltaTime * (moveSpeed * .75));
+    }
+    if(Input.GetButton("A"))
+    {
+        transform.Translate(-transform.right * Time.deltaTime * (moveSpeed * .75));
+    }
+}
+
+function LookMouse()
+{
+    if(lookLock)
+    {
+        return;
+    }
+
+    var rotateY : float = Mathf.Clamp(Input.GetAxis("Mouse Y"), minY, maxY);
+
+    if(Input.GetAxis("Mouse X"))
+    {
+        transform.Rotate(Vector3(0,Input.GetAxis("Mouse X") * lookSpeed, 0));
+    }
+    if(Input.GetAxis("Mouse Y"))
+    {
+        if(yInvertLook)
+            cameraHolder.Rotate(Vector3(Input.GetAxis("Mouse Y") * lookSpeed, 0,0));
+        else if(!yInvertLook)
+            cameraHolder.Rotate(Vector3(-Input.GetAxis("Mouse Y") * lookSpeed, 0, 0));
+    }
+    
+}
+
+function ClampLook()
+{
+   
+    if(cameraHolder.rotation.x >= minY)
+    {
+        cameraHolder.rotation.x = minY;
+    }
+}
